921a3d41b43995a60ddca6bfcfe1fe64f89941b6
unknown <akamakasin@gmail.com>
2022-03-27 22:11:38 +0700

    completed all 1 lesson

---
+++ lesson 1/confirm/index.js
@@ -0,0 +1,29 @@
+if (confirm("JavaScript появился в 1995 году?")){
+
+    alert("Верно")
+
+}else{
+
+    alert("1995")
+
+};
+
+if (confirm("Спецификация JavaScript называется ECMAScript?")){
+
+    alert("Верно")
+
+}else{
+
+    alert("ECMAScript")
+
+};
+
+if (confirm("JavaScript был создан за 1 месяц?")){
+
+    alert("за 10 дней")
+
+}else{
+
+    alert("Верно")
+    
+};
