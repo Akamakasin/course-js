4df7fee39cc52099ed2fb607ef92dfc31ee1a3fe
unknown <akamakasin@gmail.com>
2022-03-28 20:41:36 +0700

    add lesson 2

---
+++ lesson 2/car/car.js
@@ -0,0 +1,25 @@
+const carName = "Lada XRay";
+let maxCarSpeed = "180 км/ч";
+let carOwner = "Максим Морозов";
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+maxCarSpeed = "200 км/ч";
+console.log("У машины изменилась скорость", maxCarSpeed);
+
+carOwner = "Славик";
+console.log("У машины изменился владелец", carOwner);
+
+maxCarSpeed = "250 км/ч";
+console.log("У машины изменилась скорость", maxCarSpeed);
+
+carOwner = "Гений";
+console.log("У машины изменился владелец", carOwner);
+
+maxCarSpeed = "100500 км/ч";
+console.log("У машины изменилась скорость", maxCarSpeed);
+
+carOwner = "Бог гонок";
+console.log("У машины изменился владелец", carOwner);
---
+++ lesson 2/index.js
@@ -0,0 +1,16 @@
+// JavaScript код с созданием переменных
+/* 
+Автор
+Морозов Максим   
+*/
+let myName = "Максим";
+let mySurname = "Морозов";
+let myFavoriteDrink = "Снежок";
+let myFavoriteAnimal = "Черепашка";
+let myFavoriteProgrammingLanguage = "JavaScript";
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
