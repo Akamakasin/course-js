ee9a12c82965649fb063984ab79add340e5ab9c6
Максим Владимирович Морозов <MorozovMV@asmu.local>
2022-03-29 08:57:42 +0700

    add lesson 3

---
+++ lesson 3/data-types.js
@@ -0,0 +1,34 @@
+const age = 26;
+
+console.log('age', age, typeof age);
+
+const String = "good";
+
+console.log('String', String, typeof String);
+
+const Boolean = true;
+
+console.log('Boolean', Boolean, typeof Boolean);
+
+const nuLL = null;
+
+console.log('nuLL', nuLL, typeof nuLL);
+
+const Undefined = undefined;
+
+console.log('Undefined', Undefined, typeof Undefined);
+
+const Object = {
+    number: 26,
+    string: "good"
+};
+
+console.log('Object', Object, typeof Object);
+
+const BigInter = BigInt(100);
+
+console.log('BigInter', BigInter, typeof BigInter);
+
+const SYmbol = Symbol("id");
+
+console.log('SYmbol', SYmbol, typeof SYmbol);
---
+++ lesson 3/example 2/index.js
@@ -0,0 +1,17 @@
+let Number = 26;
+
+Number = "26";
+
+alert(Number);
+
+let StRing = "good";
+
+StRing = 26;
+
+alert(StRing);
+
+let BooLean = false;
+
+BooLean = null;
+
+alert(BooLean);
